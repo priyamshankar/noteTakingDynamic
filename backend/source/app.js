dd549d1cfb7b37d16fbd09402faba4f9c6c0576c
priyam <priyamshankar.5@gmail.com>
2022-02-09 16:26:52 +0530

    organising the frontend and ejs framework

--- backend/source/app.js
+++ backend/source/app.js
@@ -1,9 +1,14 @@
 const express =require("express");
 const app=express();
+const path=require("path");
 const port=process.env.PORT || 8000;
 const router=require("./database/connection");
 const dbconnection=require("./database/dbConnection");
 // const router=express.Router();
+
+const frontend_path = path.join(__dirname, "../../frontend/views");
+app.set("views", frontend_path);
+
 app.set('view engine','ejs');
 app.listen(port,()=>{
     console.log("localhost connected");
--- backend/source/database/connection.js
+++ backend/source/database/connection.js
@@ -1,12 +1,15 @@
-const express=require("express");
+const express = require("express");
 // const mongoose =require("mongoose");
-const router=new express.Router();
+const router = new express.Router();
+const app = express();
 
-router.get("/",(req,res)=>{
-    res.send("this is the home page");
+router.get("/", (req, res) => {
+    // res.send("this is the home page");
+    res.render("index")
 })
-router.get("/registration",(req,res)=>{
-    res.send("this is the registration page");
+router.get("/registration", (req, res) => {
+    // res.send("this is the registration page");
+    res.render("registrationPage");
 })
 
-module.exports=router;
+module.exports = router; 
