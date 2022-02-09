3bf3aee03a15bca66502c400bc289e5aedc13a9b
priyam <priyamshankar.5@gmail.com>
2022-02-09 21:18:20 +0530

    succesfull taking of data and updating the database

--- backend/source/database/connection.js
+++ backend/source/database/connection.js
@@ -2,7 +2,9 @@ const express = require("express");
 // const mongoose =require("mongoose");
 const router = new express.Router();
 const app = express();
-
+const userDetModel=require("./userDet");
+router.use(express.json())
+router.use(express.urlencoded({extended:false}));
 router.get("/", (req, res) => {
     // res.send("this is the home page");
     res.render("index")
@@ -11,5 +13,16 @@ router.get("/registration", (req, res) => {
     // res.send("this is the registration page");
     res.render("registrationPage");
 })
+router.post("/registration", async (req,res)=>{
+    try{
+            console.log(req.body);
+            res.render("index");
+            const saveUserDet=new userDetModel(req.body);
+            await saveUserDet.save();
+    }catch (err){
+            console.log(err);
+    }
+})
+
 
 module.exports = router; 
---
+++ backend/source/database/userDet.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const userDetSchema=new mongoose.Schema({
+    firstName:String,
+    lastName:String,
+    userName:String,
+    city:String,
+    state:String,
+    zip:Number,
+    password:String,
+    cnfmpassword:String
+})
+const userDetModel=new mongoose.model("userDetails",userDetSchema);
+module.exports=userDetModel;
