e71b1fc99669b3a310854c8e1a150c3f27b861d8
priyam <priyamshankar.5@gmail.com>
2022-02-13 02:00:02 +0530

    auth user model requires promise

--- backend/source/middleware/auth.js
+++ backend/source/middleware/auth.js
@@ -7,8 +7,8 @@ const auth = async (req, res, next) => {
         // console.log(process.env.TOKEN_KEY);
         const token = req.cookies.jwt;
         const verifyUser = jwt.verify(token, process.env.TOKEN_KEY);
-        const user=userDetModel.findOne({_id:verifyUser._id});
-        console.log(user._id);
+        const user=await userDetModel.findOne({_id:verifyUser._id});
+        console.log(user);
         next();
     } catch (err) {
         console.log(err);
